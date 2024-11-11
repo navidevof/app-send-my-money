c93ee52e056d131f05487d8fc9aa24e479c90c9a
Navidev <navi.devoficial@gmail.com>
2024-11-10 22:09:47 -0500

    Feature: add analytics

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,12 @@
 <template>
   <router-view></router-view>
 </template>
+
+<script lang="ts" setup>
+import { inject } from "@vercel/analytics";
+import { onUnmounted } from "vue";
+
+onUnmounted(() => {
+  inject();
+});
+</script>
